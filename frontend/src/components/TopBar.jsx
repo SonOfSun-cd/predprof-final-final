d241224ad7cae4f059c1dd43389e9162d5906258
SonOfSun-cd <fedorsapelkin0@gmail.com>
2026-03-15 14:07:40 +0400

    топбар

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react'
-import { Outlet, Navigate, Link } from 'react-router-dom'
-
-
-
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import { useAuth } from '../pages/auth/auth_context.jsx'
 
 export default function TopBar() {
-    const [isOpen, setIsOpen] = useState(false)
+    const { user, loading } = useAuth()
 
     return ( 
         <>
             <div className="TopBar">
-                <h1>Это бар поверх всего ваще прям всего</h1>
+                <h1>
+                    <Link to="/">Голосовой Патруль Альфа-Центавра</Link>
+                </h1>
+                <div>
+                    {!loading && !user && (
+                        <Link to="/auth/login">
+                            <button type="button">Войти</button>
+                        </Link>
+                    )}
+
+                    {!loading && user && (
+                        <>
+                            <span>{user.name}</span>
+                            {user.role === 'admin' && (
+                                <Link to="/auth/users">
+                                    <button type="button">Пользователи</button>
+                                </Link>
+                            )}
+                        </>
+                    )}
+                </div>
             </div>
-            <div class="otherdata">
+            <div className="otherdata">
                 <Outlet/>
             </div>
         </>
